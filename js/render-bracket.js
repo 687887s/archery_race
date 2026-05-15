5d95bb5af572b1adf2d94403ea337226d08de987
User <user@example.com>
2026-05-15 22:58:17 +0800

    Aggressively fix team name wrapping issues on mobile. Update version to 1.6.2

--- js/render-bracket.js
+++ js/render-bracket.js
@@ -1,4 +1,4 @@
-/** v1.6.1 **/
+/** v1.6.2 **/
 export function renderBracket(containerId, matches) {
     const container = document.getElementById(containerId);
     container.innerHTML = '';
--- js/render-standings.js
+++ js/render-standings.js
@@ -1,4 +1,4 @@
-/** v1.6.1 **/
+/** v1.6.2 **/
 const unitColors = {
     '國立臺灣大學': '#38bdf8', // Light Blue
     '陽明射箭社': '#f472b6',    // Pink
