cfcd86cfacccd0311298797175c2869ad9247f03
User <user@example.com>
2026-05-15 22:51:59 +0800

    Fix mobile UI: restore member targets, fix container height (100dvh), wrap long team names, and add bottom padding. Update version to 1.6.1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-import { DataHandler } from './data-handler.js?v=1.6.0';
-import { renderStandings } from './render-standings.js?v=1.6.0';
-import { renderBracket } from './render-bracket.js?v=1.6.0';
+import { DataHandler } from './data-handler.js?v=1.6.1';
+import { renderStandings } from './render-standings.js?v=1.6.1';
+import { renderBracket } from './render-bracket.js?v=1.6.1';
 
 const handler = new DataHandler();
 
--- js/data-handler.js
+++ js/data-handler.js
@@ -1,4 +1,4 @@
-/** v1.6.0 **/
+/** v1.6.1 **/
 export class DataHandler {
     constructor() {
         this.players = [];
